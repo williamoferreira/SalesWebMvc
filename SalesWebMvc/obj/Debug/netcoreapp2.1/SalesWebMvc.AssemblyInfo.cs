65c3dd4c9665e0c34cc0102c174beb5345df0bfe
William de Oliveira Ferreira <williamoferreira@outlook.com>
2024-05-12 21:03:19 -0300

    Aula 254 - Scaffolding para criar view Index para Departments está falhando

--- SalesWebMvc/obj/Debug/netcoreapp2.1/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/netcoreapp2.1/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8b0c39f1211bd16c93815abc796e41ae432f92")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SalesWebMvc/obj/Debug/netcoreapp2.1/SalesWebMvc.RazorTargetAssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/netcoreapp2.1/SalesWebMvc.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8b0c39f1211bd16c93815abc796e41ae432f92")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
